76bc01150c6160093596ec46fd05b9c071a267f6
JKachele <justin@kachele.com>
2023-02-08 15:45:42 -0500

    Added Lab 2

---
+++ labs/lab2/jkachele_258_lab2.py
@@ -0,0 +1,9 @@
+def calculate_hypotenuse(a, b):
+    cSquared = a ** 2 + b ** 2
+    c = cSquared ** 0.5
+    return c
+
+print("Hypoenuse Calculator!")
+a = float(input("Input the lengthe of leg a of the right triangle: "))
+b = float(input("Input the lengthe of leg b of the right triangle: "))
+print("The length of the hypotenuse is:", calculate_hypotenuse(a, b))
